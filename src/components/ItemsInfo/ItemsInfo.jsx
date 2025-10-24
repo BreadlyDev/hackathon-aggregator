aa8f5cedd3d5167436ca5c7c70dba236b8313c99
Aidarbek4 <aidarbek.kurmankozhoev@gmail.com>
2025-10-24 21:47:22 +0200

    Items information Component added

---
+++ src/components/ItemsInfo/ItemsInfo.jsx
@@ -0,0 +1,89 @@
+import React, { useEffect, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, FreeMode, Thumbs } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/navigation";
+import "swiper/css/thumbs";
+import classes from "./ItemsInfo.module.scss";
+
+export default function ItemsInfo() {
+  const [products, setProducts] = useState([]);
+  const [thumbsSwiper, setThumbsSwiper] = useState(null);
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  useEffect(() => {
+    fetch("/jdsports_products.json")
+      .then((res) => res.json())
+      .then((data) => setProducts(data))
+      .catch((err) => console.error(err));
+  }, []);
+
+  if (!products.length) return null;
+
+  const activeProduct = products[activeIndex];
+
+  return (
+    <div className={classes.ItemsInfo}>
+      <h2 className={classes.ItemsInfo__ShopName}>Shop Name</h2>
+
+      {/* Главный слайдер */}
+      <Swiper
+        loop={true}
+        spaceBetween={10}
+        navigation={true}
+        thumbs={{ swiper: thumbsSwiper }}
+        modules={[FreeMode, Navigation, Thumbs]}
+        className={classes.ItemInfoSwiper}
+        onSlideChange={(swiper) => setActiveIndex(swiper.realIndex)}
+      >
+        {products.map((item, index) => (
+          <SwiperSlide key={index}>
+            <a href={item.ref_item} target="_blank" rel="noopener noreferrer">
+              <img src={item.img_url} alt={item.title} />
+            </a>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+
+      {/* Миниатюры */}
+      <Swiper
+        onSwiper={setThumbsSwiper}
+        loop={true}
+        spaceBetween={10}
+        slidesPerView={5}
+        freeMode={true}
+        watchSlidesProgress={true}
+        modules={[FreeMode, Navigation, Thumbs]}
+        className={classes.ItemInfoSwiper + "_thumbs"}
+      >
+        {products.map((item, index) => (
+          <SwiperSlide key={index}>
+            <img src={item.img_url} alt={item.title} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+
+      {/* Детали активного продукта */}
+      <div className={classes.ItemsInfo__Details}>
+        <h4 className={classes.ItemsInfo__Details__Title}>Details:</h4>
+
+        <div className={classes.ItemsInfo__Details_Row}>
+          <span>Size:</span>
+          <ul className={classes.ItemsInfo__Details_Size}>
+            {activeProduct.size.map((s) => (
+              <li key={s}>{s}</li>
+            ))}
+          </ul>
+        </div>
+
+        <div className={classes.ItemsInfo__Details_Row}>
+          <span>Color:</span>
+          <ul className={classes.ItemsInfo__Details_Color}>
+            <li>{activeProduct.color}</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
